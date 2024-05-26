afdc9daf542e7e0b60a1d2bf71978996551af11c
Eduard <eduard.dev.de@gmail.com>
2024-05-26 14:41:07 +0200

    add icon to input, remove classname prop

--- src/shared/ui/input/Input.tsx
+++ src/shared/ui/input/Input.tsx
@@ -1,27 +1,31 @@
 import { useId, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 
-import { Icon, classNames } from '@/shared'
+import { Icon, IconName, classNames } from '@/shared'
 
 import cl from './Input.module.scss'
 
 export const Input = ({
     label,
-    className,
     noTranslate,
-    placeholder,
-    error,
+	placeholder,
+	error,
 	type = 'text',
-    ...props
+    variant = 'default',
+	icon,
+	onClickIcon,
+	...props
 }: {
-    label?: string
-    className?: string
+	label?: string
 	noTranslate?: boolean
 	placeholder: string
 	error?: string
 	type?: 'text' | 'password' | 'tel' | 'date' | 'email'
+	variant?: 'search' | 'default' | 'auth'
 	required?: boolean
 	disabled?: boolean
+	icon?: IconName
+	onClickIcon?: () => void
 }) => {
     const [isPasswordShow, setIsPasswordShow] = useState(false)
 
@@ -42,10 +46,22 @@ export const Input = ({
 				</label>
 			)}
 			<div className={cl.root__input__wrapper}>
-                <input
-                    className={classNames(
+                {variant === 'search' && (
+					<button
+						onClick={onClickIcon}
+						className={classNames(
+							cl.root__button,
+							cl.root__button_search
+						)}>
+						<Icon name="Search" />
+					</button>
+				)}
+				<input
+					className={classNames(
 						cl.root__input,
-						className ?? '',
+						variant === 'search' ? cl.root__input_search : '',
+						variant === 'auth' ? cl.root__input_auth : '',
+						icon || type === 'password' ? cl.root__input_icon : '',
 						error ? cl.root__input_error : ''
                     )}
 					id={labelId}
@@ -57,10 +73,23 @@ export const Input = ({
 					<button
 						className={classNames(
 							cl.root__button,
-							isPasswordShow ? cl.root__button_active : ''
+							cl.root__button_password,
+							isPasswordShow
+								? cl.root__button_password_active
+								: ''
 						)}
 						onClick={() => setIsPasswordShow(!isPasswordShow)}>
 						<Icon name="Eye" />
+					</button>
+				)}
+				{icon && (
+					<button
+						className={classNames(
+							cl.root__button,
+							cl.root__button_custom
+						)}
+						onClick={onClickIcon}>
+						<Icon name={icon} />
 					</button>
 				)}
 			</div>
