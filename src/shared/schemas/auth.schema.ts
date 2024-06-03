84f732501c206cf6aeca9e2125ec567e081dba21
Eduard <eduard.dev.de@gmail.com>
2024-06-03 06:17:43 +0200

    added new validation to password

--- src/shared/schemas/auth.schema.ts
+++ src/shared/schemas/auth.schema.ts
@@ -7,9 +7,27 @@ export const AuthSchema = () => {
 	const { t } = useTranslation()
 
 	return z.object({
-		email: z.string().email(t('invalid_email_address')),
+		email: z.string().email(t('invalid email address')),
 		password: z
 			.string()
-			.min(8, t('password_must_be_at_least_8_characters_long'))
+			.min(8, t('password must be at least 8 characters long'))
+			.refine(value => /[a-z]/.test(value), {
+				message: t(
+					'password must contain at least one lowercase letter'
+				)
+			})
+			.refine(value => /[A-Z]/.test(value), {
+				message: t(
+					'password must contain at least one uppercase letter'
+				)
+			})
+			.refine(value => /[0-9]/.test(value), {
+				message: t('password must contain at least one number')
+			})
+			.refine(value => /[^A-Za-z0-9]/.test(value), {
+				message: t(
+					'password must contain at least one special character'
+				)
+			})
 	})
 }
