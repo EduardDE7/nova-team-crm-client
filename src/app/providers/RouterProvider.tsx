f4a21a9260688517e4ba4cc2ac331257e36ef0e4
andreyfeel83 <andreyfeel83@gmail.com>
2024-06-14 14:10:08 +0300

    added checkedRoutes function

--- src/app/providers/RouterProvider.tsx
+++ src/app/providers/RouterProvider.tsx
@@ -4,41 +4,30 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { privateRoutes, publicRoutes } from '@/shared'
 import { useIsAuthorizedValue } from '@/shared/api/is-authorized'
 
-//solve type conflict problem
 type TRouteItem = {
     Page: FC
     path: string
 }
+
 //add the function correctly
-// function checkedRoutes(route: TRouteItem[]) {
-//     route.map(({ Page, path }: TRouteItem) => (
-//         <Route
-//             key={path}
-//             path={path}
-//             element={<Page />}
-//         />
-//     ))
-// }
+const checkedRoutes = (route: TRouteItem[]) => {
+    const routes = route.map(({ Page, path }: TRouteItem) => (
+        <Route
+            key={path}
+            path={path}
+            element={<Page />}
+        />
+    ))
+    return <>{routes}</>
+}
 
 export const RouterProvider = () => {
     return (
         <BrowserRouter>
             <Routes>
                 {useIsAuthorizedValue()
-                    ? privateRoutes.map(({ Page, path }: TRouteItem) => (
-                          <Route
-                              key={path}
-                              path={path}
-                              element={<Page />}
-                          />
-                      ))
-                    : publicRoutes.map(({ Page, path }: TRouteItem) => (
-                          <Route
-                              key={path}
-                              path={path}
-                              element={<Page />}
-                          />
-                      ))}
+                    ? checkedRoutes(privateRoutes)
+                    : checkedRoutes(publicRoutes)}
             </Routes>
         </BrowserRouter>
     )
