3c47068a1755953e747561527808100e8a859f9b
myNewGitAcc <newregwithemail@gmail.com>
2024-07-05 15:51:21 +0300

    linter pre-commit autofix

--- src/shared/ui/label/Label.stories.tsx
+++ src/shared/ui/label/Label.stories.tsx
@@ -3,23 +3,23 @@ import type { Meta, StoryObj } from '@storybook/react'
 import { Label } from './Label'
 
 const meta: Meta<typeof Label> = {
-	title: 'shared/Label',
-	component: Label,
-	tags: ['autodocs'],
-	argTypes: {
-		isError: {
-			type: 'boolean',
-			description: 'is error'
-		},
-		htmlFor: {
-			type: 'string',
-			description: 'htmlFor'
-		},
-		text: {
-			type: 'string',
-			description: 'text'
-		}
-	}
+    title: 'shared/Label',
+    component: Label,
+    tags: ['autodocs'],
+    argTypes: {
+        isError: {
+            type: 'boolean',
+            description: 'is error'
+        },
+        htmlFor: {
+            type: 'string',
+            description: 'htmlFor'
+        },
+        text: {
+            type: 'string',
+            description: 'text'
+        }
+    }
 } satisfies Meta<typeof Label>
 
 export default meta
@@ -27,14 +27,14 @@ export default meta
 type Story = StoryObj<typeof Label>
 
 export const Default: Story = {
-	args: {
-		text: 'Label'
-	}
+    args: {
+        text: 'Label'
+    }
 }
 
 export const WithError: Story = {
-	args: {
-		isError: true,
-		text: 'Label'
-	}
+    args: {
+        isError: true,
+        text: 'Label'
+    }
 }
