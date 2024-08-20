93703e66aa1c38465eaefc8ff1729f6232fccd0d
andreyfeel83 <andreyfeel83@gmail.com>
2024-08-20 19:22:59 +0300

    some more fixes

--- src/shared/ui/checkbox/Checkbox.stories.tsx
+++ src/shared/ui/checkbox/Checkbox.stories.tsx
@@ -1,42 +1,3 @@
-// import type { Meta, StoryObj } from '@storybook/react'
-// import { Checkbox } from './Checkbox'
-
-// const meta = {
-//     title: 'shared/Checkbox',
-//     component: Checkbox,
-//     parameters: {
-//         layout: 'centered'
-//     },
-//     tags: ['autodocs'],
-//     argTypes: {
-//         checked: {
-//             type: 'boolean',
-//             description: 'checkbox by default'
-//         },
-//         onChange: {
-//             type: 'function',
-//             description: `takes as an argument 'checked' and returns a result depending on the value`,
-//             action: 'clicked'
-//         }
-//     }
-// } satisfies Meta<typeof Checkbox>
-
-// export default meta
-// type Story = StoryObj<typeof meta>
-
-// export const Default: Story = {
-//     args: {
-//         checked: false,
-//         onChange: checked => {}
-//     }
-// }
-
-// export const Checked: Story = {
-//     args: {
-//         checked: true,
-//         onChange: checked => {}
-//     }
-// }
 import { StoryObj, Meta } from '@storybook/react'
 import { useArgs } from '@storybook/preview-api'
 import { Checkbox } from './Checkbox'
@@ -73,8 +34,8 @@ export const Default: Story = {
         return (
             <Checkbox
                 {...arg}
-                onChange={onChange}
                 checked={checked}
+                onChange={onChange}
             />
         )
     }
